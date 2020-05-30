48c57af4a907fa5cdca6e26295e49b2cc86b93b7
JabaHum <jabajnan8@gmail.com>
2020-05-30 19:28:11 +0300

    Added Calender on "to" and "From" Buttons

--- app/src/main/java/com/example/covidstatusapp/countrydetails/DetailsActivity.java
+++ app/src/main/java/com/example/covidstatusapp/countrydetails/DetailsActivity.java
@@ -1,13 +1,19 @@
 package com.example.covidstatusapp.countrydetails;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.covidstatusapp.R;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -16,6 +22,9 @@ public class DetailsActivity extends AppCompatActivity {
     Button btnFrom;
     Button btnTo;
     RecyclerView mRecyclerView;
+
+    private int mYear, mMonth, mDay;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,7 +35,34 @@ public class DetailsActivity extends AppCompatActivity {
         btnTo = findViewById(R.id.btn_to);
         mRecyclerView = findViewById(R.id.recycler_view_details);
 
+        btnFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDate(btnFrom);
+            }
+        });
 
+        btnTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDate(btnTo);
+            }
+        });
+    }
 
+    private void setDate(final Button btn) {
+        Calendar calendar = Calendar.getInstance();
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(DetailsActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        btn.setText(String.format(Locale.US, "%d-%s-%s", year, month, dayOfMonth));
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
     }
 }
