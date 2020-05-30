39b6b47b43a6ccb43fa63935cfc2e7a8e1e3ec65
JabaHum <jabajnan8@gmail.com>
2020-05-30 18:33:41 +0300

    Added Details Module

---
+++ app/src/main/java/com/example/covidstatusapp/countrydetails/DetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.covidstatusapp.countrydetails;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
