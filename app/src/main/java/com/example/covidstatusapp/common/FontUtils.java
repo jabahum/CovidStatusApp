f13eef02fe07430b9f89a671f26ba24887c96684
JabaHum <jabajnan8@gmail.com>
2020-06-25 01:23:10 +0300

    Edited SwipeRefreshLayout code

---
+++ app/src/main/java/com/example/covidstatusapp/common/FontUtils.java
@@ -0,0 +1,77 @@
+package com.example.covidstatusapp.common;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class FontUtils {
+
+    private static Typeface typefaceBold, typefaceRegular,typefaceLight;
+    private static FontUtils fontUtils;
+
+    public static FontUtils getFontUtils(Context context) {
+        if (fontUtils == null) {
+            fontUtils = new FontUtils();
+            typefaceBold = Typeface.createFromAsset(context.getAssets(), "fonts/open_sans_bold.ttf");
+            typefaceRegular = Typeface.createFromAsset(context.getAssets(), "fonts/open_sans_regular.ttf");
+            typefaceLight = Typeface.createFromAsset(context.getAssets(),"fonts/open_sans_light.ttf");
+
+        }
+        return fontUtils;
+    }
+
+    public void setTextViewRegularFont(TextView textView) {
+        textView.setTypeface(typefaceRegular);
+    }
+
+    public void setTextViewBoldFont(TextView textView) {
+        textView.setTypeface(typefaceBold);
+    }
+
+    public void setTextViewLightFont(TextView textView) {
+        textView.setTypeface(typefaceLight);
+    }
+
+    public void setButtonRegularFont(Button button) {
+        button.setTypeface(typefaceRegular);
+    }
+
+    public void setButtonBoldFont(Button button) {
+        button.setTypeface(typefaceBold);
+    }
+
+    public void setButtonLightFont(Button button) {
+        button.setTypeface(typefaceLight);
+    }
+
+    public void setEditTextRegularFont(EditText editText) {
+        editText.setTypeface(typefaceRegular);
+    }
+
+    public void setEditTextBoldFont(EditText editText) {
+        editText.setTypeface(typefaceBold);
+    }
+
+    public void setEditTextLightFont(EditText editText) {
+        editText.setTypeface(typefaceLight);
+    }
+
+    public Typeface getTypefaceRegular() {
+        return typefaceRegular;
+    }
+
+    public Typeface getTypefaceBold() {
+        return typefaceBold;
+    }
+
+    public Typeface getTypefaceLight() {
+        return typefaceLight;
+    }
+
+    public void setRadioButtonRegularFont(RadioButton radioButton) {
+        radioButton.setTypeface(typefaceRegular);
+    }
+}
--- app/src/main/java/com/example/covidstatusapp/fragments/StatisticsFragment.java
+++ app/src/main/java/com/example/covidstatusapp/fragments/StatisticsFragment.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.covidstatusapp.R;
+import com.example.covidstatusapp.adapters.CustomPagerAdapter;
+import com.example.covidstatusapp.common.FontUtils;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
 
 public class StatisticsFragment extends Fragment {
+    private SmartTabLayout mTabLayout;
+    private ViewPager mViewPager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -20,5 +29,37 @@ public class StatisticsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mTabLayout = view.findViewById(R.id.tl_main_statistics);
+        mViewPager = view.findViewById(R.id.vp_region);
+
+        init();
+    }
+
+    private void init() {
+        mViewPager.setAdapter(new CustomPagerAdapter(getChildFragmentManager(), getContext()));
+
+        mTabLayout.setCustomTabView(new SmartTabLayout.TabProvider() {
+            @Override
+            public View createTabView(ViewGroup container, int position, PagerAdapter adapter) {
+                LayoutInflater inflater = LayoutInflater.from(container.getContext());
+                View tab = inflater.inflate(R.layout.layout_custom_tab, container, false);
+                TextView customText = tab.findViewById(R.id.textViewCustomTab);
+                FontUtils.getFontUtils(getActivity()).setTextViewRegularFont(customText);
+                switch (position) {
+                    case 0:
+                        customText.setText(adapter.getPageTitle(position));
+                        break;
+                    case 1:
+                        customText.setText(adapter.getPageTitle(position));
+                        break;
+                    default:
+                        throw new IllegalStateException("Invalid position: " + position);
+                }
+                return tab;
+            }
+        });
+
+        mTabLayout.setViewPager(mViewPager);
+        mViewPager.setOffscreenPageLimit(2);
     }
 }
