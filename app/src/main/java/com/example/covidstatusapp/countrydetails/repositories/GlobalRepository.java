8933b215f677c3de428134c4dec2752f1a9e9290
JabaHum <jabajnan8@gmail.com>
2020-06-25 16:59:00 +0300

    Edited SwipeRefreshLayout code

--- app/src/main/java/com/example/covidstatusapp/countrydetails/repositories/GlobalRepository.java
+++ app/src/main/java/com/example/covidstatusapp/countrydetails/repositories/GlobalRepository.java
@@ -1,4 +1,20 @@
 package com.example.covidstatusapp.countrydetails.repositories;
 
+import com.example.covidstatusapp.network.APIClient;
+import com.example.covidstatusapp.network.APIinterface;
+
 public class GlobalRepository {
+    private static GlobalRepository globalRepository;
+    private APIinterface apIinterface;
+
+    public GlobalRepository() {
+        apIinterface = APIClient.getClient().create(APIinterface.class);
+    }
+
+    public static GlobalRepository getInstance() {
+        if (globalRepository == null) {
+            globalRepository = new GlobalRepository();
+        }
+        return globalRepository;
+    }
 }
--- app/src/main/java/com/example/covidstatusapp/countrydetails/repositories/MyCountryRepository.java
+++ app/src/main/java/com/example/covidstatusapp/countrydetails/repositories/MyCountryRepository.java
@@ -1,4 +1,20 @@
 package com.example.covidstatusapp.countrydetails.repositories;
 
+import com.example.covidstatusapp.network.APIClient;
+import com.example.covidstatusapp.network.APIinterface;
+
 public class MyCountryRepository {
+    private static MyCountryRepository myCountryRepository;
+    private APIinterface apIinterface;
+
+    public MyCountryRepository() {
+        apIinterface = APIClient.getClient().create(APIinterface.class);
+    }
+
+    public static MyCountryRepository getInstance() {
+        if (myCountryRepository == null) {
+            myCountryRepository = new MyCountryRepository();
+        }
+        return myCountryRepository;
+    }
 }
