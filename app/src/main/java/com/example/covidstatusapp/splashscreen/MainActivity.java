de77e6ac32e7eb357d1127e282cf5823712d13ff
JabaHum <jabajnan8@gmail.com>
2020-06-28 15:54:06 +0300

    Edited SwipeRefreshLayout code

--- app/src/main/java/com/example/covidstatusapp/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/covidstatusapp/splashscreen/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         pageTitle = findViewById(R.id.title);
         pageSubTitle = findViewById(R.id.subTitle);
