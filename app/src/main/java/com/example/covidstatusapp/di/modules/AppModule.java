433428e3f85ca5dde368022f1af93585fe793e91
JabaHum <jabajnan8@gmail.com>
2020-07-03 00:34:41 +0300

    Removed Unused Imports

--- app/src/main/java/com/example/covidstatusapp/di/modules/AppModule.java
+++ app/src/main/java/com/example/covidstatusapp/di/modules/AppModule.java
@@ -6,20 +6,16 @@ import com.example.covidstatusapp.ui.utils.Constants;
 import com.example.covidstatusapp.ui.utils.sharedPreferences.AppPreferenceManagerHelper;
 import com.example.covidstatusapp.ui.utils.sharedPreferences.PreferenceManager;
 
-import java.util.concurrent.TimeUnit;
-
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-import timber.log.Timber;
 
 @Module
 public class AppModule {
@@ -30,6 +26,19 @@ public class AppModule {
                 .baseUrl(Constants.base_Url)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(setLogging())
+                .build();
+    }
+
+
+    @Provides
+    @Singleton
+    static OkHttpClient setLogging() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        return new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
                 .build();
     }
 
