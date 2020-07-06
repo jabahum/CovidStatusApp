b640120fe8174801711c246aba0baaa6d51e554b
JabaHum <jabajnan8@gmail.com>
2020-07-06 17:46:04 +0300

    Added Dialogs for Prevention Information

--- app/src/main/java/com/example/covidstatusapp/ui/utils/BindingUtils/BindingUtils.java
+++ app/src/main/java/com/example/covidstatusapp/ui/utils/BindingUtils/BindingUtils.java
@@ -15,4 +15,10 @@ public final class BindingUtils {
         }
     }
 
+    @BindingAdapter({"content"})
+    public static void textString(TextView tv, String value) {
+        if (value != null) {
+            tv.setText(value);
+        }
+    }
 }
