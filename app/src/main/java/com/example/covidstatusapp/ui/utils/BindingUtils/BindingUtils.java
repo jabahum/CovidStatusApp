1519161c6448f5784c650db831f843870ff99345
JabaHum <jabajnan8@gmail.com>
2020-07-06 15:55:23 +0300

    Added Dialogs for Prevention Information

---
+++ app/src/main/java/com/example/covidstatusapp/ui/utils/BindingUtils/BindingUtils.java
@@ -0,0 +1,18 @@
+package com.example.covidstatusapp.ui.utils.BindingUtils;
+
+import android.widget.TextView;
+
+import androidx.databinding.BindingAdapter;
+
+
+public final class BindingUtils {
+
+
+    @BindingAdapter({"content"})
+    public static void setContent(TextView tv, String value) {
+        if (value != null) {
+            tv.setText(value);
+        }
+    }
+
+}
